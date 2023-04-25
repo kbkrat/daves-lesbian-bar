329621ee4969e6a5677bf7a9722239bf5d9670aa
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-04-25 15:53:56 -0400

    feat: Add data and read data into Index

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,3 +1,18 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {}
+interface MapMarker {
+  title: string;
+  lat: number;
+  long: number;
+  location: string;
+  address: string;
+  link: string;
+}
+
+export default class IndexRoute extends Route {
+  async model() {
+    const response = await fetch('/data.json');
+    const data: MapMarker[] = await response.json();
+    return data;
+  }
+}
